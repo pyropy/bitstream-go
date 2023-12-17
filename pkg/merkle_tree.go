d79c2b1291fb01ed7298d95953d07d33750a2c84
Srdjan S. <stankovic.srdjo@gmail.com>
2023-12-17 15:12:27 +0100

    Refactor merkle tree

--- pkg/merkle_tree.go
+++ pkg/merkle_tree.go
@@ -3,45 +3,67 @@ package bitstream
 import "crypto/sha256"
 
 type Hasher interface {
-	Hash() []byte
-}
-
-type Tree struct {
-	Root *Node
-}
-
-func NewTree() *Tree {
-	return &Tree{
-		Root: &Node{},
-	}
-}
-
-func (t *Tree) Insert(n *Node) {
-	t.Root.Leaves = append(t.Root.Leaves, n)
-}
-
-func (t *Tree) Hash() []byte {
-	return t.Root.Hash()
+	GetHash() []byte
 }
 
 type Node struct {
-	Data   []byte
-	Leaves []*Node
+	Data  []byte
+	Hash  []byte
+	Left  *Node
+	Right *Node
 }
 
-func (n *Node) Hash() []byte {
+func (n *Node) GetHash() []byte {
+	if len(n.Hash) > 0 {
+		return n.Hash
+	}
+
 	h := sha256.New()
-	if n.IsLeaf() {
-		h.Write(n.Data)
+	h.Write(n.Left.GetHash())
+
+	if n.Right != nil {
+		h.Write(n.Right.GetHash())
 	} else {
-		for _, l := range n.Leaves {
-			h.Write(l.Hash())
-		}
+		h.Write(n.Left.GetHash())
 	}
 
 	return h.Sum(nil)
 }
 
-func (n *Node) IsLeaf() bool {
-	return len(n.Leaves) == 0
+func NewTree(leaves [][]byte) *Node {
+	if len(leaves) == 0 {
+		return &Node{Hash: make([]byte, 32)}
+	}
+
+	nodes := make([]Node, len(leaves))
+
+	for i, leaf := range leaves {
+		h := sha256.New()
+		h.Write(leaf)
+
+		nodes[i] = Node{Data: leaf, Hash: h.Sum(nil)}
+	}
+
+	for len(nodes) > 1 {
+		var newLevel []Node
+
+		for j := 0; j < len(nodes); j += 2 {
+			left := &nodes[j]
+			right := &nodes[j]
+
+			if j+1 < len(nodes) {
+				right = &nodes[j+1]
+			}
+
+			h := sha256.New()
+			h.Write(left.Hash)
+			h.Write(right.Hash)
+
+			newLevel = append(newLevel, Node{Hash: h.Sum(nil), Left: left, Right: right})
+		}
+
+		nodes = newLevel
+	}
+
+	return &nodes[0]
 }
--- pkg/merkle_tree_test.go
+++ pkg/merkle_tree_test.go
@@ -8,40 +8,55 @@ import (
 func TestTree(t *testing.T) {
 	t.Parallel()
 
-	left := &Node{
-		Data: []byte("hello"),
-	}
-
-	right := &Node{
-		Data: []byte("world"),
-	}
-
-	tree := Tree{
-		Root: &Node{
-			Leaves: []*Node{left, right},
-		},
-	}
-
-	nodes := []struct {
-		node   Hasher
+	tests := []struct {
+		leaves [][]byte
 		expect string
 	}{
 		{
-			node:   left,
+			leaves: [][]byte{},
+			expect: "0000000000000000000000000000000000000000000000000000000000000000",
+		},
+		{
+			leaves: [][]byte{
+				[]byte("hello"),
+			},
 			expect: "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
 		},
 		{
-			node:   right,
+			leaves: [][]byte{
+				[]byte("world"),
+			},
 			expect: "486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7",
 		},
 		{
-			node:   tree.Root,
+			leaves: [][]byte{
+				[]byte("hello"),
+				[]byte("world"),
+			},
 			expect: "7305db9b2abccd706c256db3d97e5ff48d677cfe4d3a5904afb7da0e3950e1e2",
+		},
+		{
+			leaves: [][]byte{
+				[]byte("hello"),
+				[]byte("world"),
+				[]byte("world"),
+			},
+			expect: "060f35066ef7e1db64e560fcdcf9f55955fb942671e3ecc79d3fb8adefb79bcb",
+		},
+		{
+			leaves: [][]byte{
+				[]byte("hello"),
+				[]byte("world"),
+				[]byte("hello"),
+				[]byte("world"),
+			},
+			expect: "f1159918525d2c95e89b7bcd62bc3da6297a49402e85d26e21bf08e5eace81c0",
 		},
 	}
 
-	for _, tt := range nodes {
-		got := tt.node.Hash()
+	for _, tt := range tests {
+		tree := NewTree(tt.leaves)
+		got := tree.GetHash()
 		if fmt.Sprintf("%x", got) != tt.expect {
 			t.Errorf("got %x, expected %s", got, tt.expect)
 		}
