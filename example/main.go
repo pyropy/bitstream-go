af0d316f4471ee9633ee373f8dd0d4b4dc8454f9
Srdjan S <stankovic.srdjo@gmail.com>
2023-12-26 16:35:18 +0100

    Add encrypt example

--- example/main.go
+++ example/main.go
@@ -1,8 +1,66 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	bitstream "github.com/pyropy/bitstream-go/pkg"
+	"io"
+	"log"
+	"os"
+)
+
+var (
+	inPath    string
+	outPath   string
+	preimage  string
+	chunkSize int64
+)
+
+func init() {
+	flag.StringVar(&inPath, "in", "", "File to encrypt/decrypt")
+	flag.StringVar(&outPath, "out", "", "Path to output encrypted/decrypted file")
+	flag.StringVar(&preimage, "preimage", "", "Preimage to use for encryption/decryption")
+	flag.Int64Var(&chunkSize, "size", 32, "Size of chunks to encrypt/decrypt")
+}
 
 // TODO: Implement encrypt and decrypt cli commands
 func main() {
-	log.Println("Nothing to see here, yet..")
+	flag.Parse()
+
+	err := encrypt(inPath, outPath)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func encrypt(inPath, outPath string) error {
+	f, err := os.Open(inPath)
+	if err != nil {
+		return err
+	}
+
+	encryptedFile, tree, err := bitstream.EncryptFile([]byte(preimage), f, chunkSize)
+	if err != nil {
+		return err
+	}
+
+	l := fmt.Sprintf("Merkle tree hash 0x%x", tree.GetHash())
+	log.Println(l)
+
+	b, err := io.ReadAll(encryptedFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(outPath, b, 0644)
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
