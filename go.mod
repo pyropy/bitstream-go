324ac606becda23c32cc87f091a3605dc2807f57
Srdjan S <stankovic.srdjo@gmail.com>
2024-01-20 17:28:38 +0100

    Add btcd pkg with secp256k1 implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/pyropy/bitstream-go
 
 go 1.21.3
+
+require github.com/btcsuite/btcd/btcec/v2 v2.3.2
+
+require (
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+)
--- pkg/encrypt.go
+++ pkg/encrypt.go
@@ -2,24 +2,50 @@ package bitstream
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"io"
 )
 
-//func Encrypt(paymentHash []byte, preimage []byte, inFile io.ReadSeeker, outFile io.Writer, chunkSize int64) error {
-//	encrypted, tree, err := EncryptFile(preimage, inFile, chunkSize)
-//	if err != nil {
-//		return err
-//	}
-//
-//	encryptedRoot := tree.GetHash()
-//	// TODO: Compute Schnorr signature from message
-//	message := append(encryptedRoot, paymentHash...)
-//
-//	outFile.Write(paymentHash)
-//
-//}
+type EncryptedFile interface {
+	io.WriterTo
+	io.Reader
+}
 
-func EncryptFile(preimage []byte, file io.ReadSeeker, chunkSize int64) (io.Reader, *MerkleTree, error) {
+func Encrypt(pk *btcec.PrivateKey, paymentHash []byte, preimage []byte, inFile io.ReadSeeker, outFile io.Writer, chunkSize int64) error {
+	encrypted, tree, err := EncryptFile(preimage, inFile, chunkSize)
+	if err != nil {
+		return err
+	}
+
+	encryptedRoot := tree.GetHash()
+	message := append(encryptedRoot, paymentHash...)
+
+	sig, err := schnorr.Sign(pk, message)
+	if err != nil {
+		return err
+	}
+
+	_, err = outFile.Write(sig.Serialize())
+	if err != nil {
+		return fmt.Errorf("failed to write schnorr sig: %w", err)
+	}
+
+	_, err = outFile.Write(paymentHash)
+	if err != nil {
+		return fmt.Errorf("failed to write payment hash: %w", err)
+	}
+
+	_, err = encrypted.WriteTo(outFile)
+	if err != nil {
+		return fmt.Errorf("failed to write encrypted file: %w", err)
+	}
+
+	return nil
+}
+
+func EncryptFile(preimage []byte, file io.ReadSeeker, chunkSize int64) (EncryptedFile, *MerkleTree, error) {
 	chunks, err := ChunkFile(file, chunkSize)
 	if err != nil {
 		return nil, nil, err
